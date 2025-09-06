049828a22b71bacdd26706ffbdd803b2dbd056b8
Max-Changg <mchang900@gmail.com>
2025-09-05 17:36:22 -0700

    added ignore build errors

--- bouncer_app/next.config.ts
+++ bouncer_app/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   trailingSlash: true,
   images: {
     unoptimized: true
